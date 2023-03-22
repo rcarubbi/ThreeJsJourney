97a117c1331356ed230c507558a3b29af5a9b86d
Raphael Carubbi Neto <rcarubbi@gmail.com>
2023-03-22 20:11:40 +0000

    Add Lesson 03 and Starter pack for Lesson 4

---
+++ 03-BasicScene/script.js
@@ -0,0 +1,32 @@
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry()
+
+const red = 0xff0000
+const material = new THREE.MeshBasicMaterial({ color: red })
+
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+// Camera
+const fieldOfViewAngles = 75
+const sizes = {
+    width: 800,
+    height: 600
+}
+const camera = new THREE.PerspectiveCamera(fieldOfViewAngles, sizes.width / sizes.height)
+camera.position.z = 3
+
+scene.add(camera)
+
+
+// renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+    canvas
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
