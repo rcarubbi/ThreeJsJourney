04bb4c5c9eeb2b73bfb3fe429547f407fc954ffe
Raphael Carubbi Neto <rcarubbi@gmail.com>
2023-03-24 00:23:30 +0000

    complete lesson 11

--- 11-Textures/src/script.js
+++ 11-Textures/src/script.js
@@ -1,6 +1,58 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+
+// // image
+// const image = new Image()
+// const texture = new THREE.Texture(image);
+
+// image.addEventListener('load', () => {
+//     texture.needsUpdate = true
+// })
+// image.src = "/textures/door/color.jpg"
+
+// Texture Loader
+const loadingManager = new THREE.LoadingManager()
+
+loadingManager.onStart = () => {
+    console.log('onStart')
+}
+
+loadingManager.onProgress = () => {
+    console.log('onProgress')
+}
+
+loadingManager.onLoad = () => {
+    console.log('onLoad')
+
+}
+
+loadingManager.onError = () => {
+    console.log('onError')
+}
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg');
+const colorTexture = textureLoader.load('/textures/minecraft.png');
+const heightTexture = textureLoader.load('/textures/door/height.jpg');
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+const normalTexture = textureLoader.load('/textures/door/normal.jpg');
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.RepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping
+
+// colorTexture.rotation = Math.PI * .25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
+
 /**
  * Base
  */
@@ -14,9 +66,13 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+
+
 
 /**
  * Sizes
@@ -26,8 +82,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -69,8 +124,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
