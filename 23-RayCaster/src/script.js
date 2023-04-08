027dd94be33a7071ea034a412b03bc5ebecd5ac5
Raphael Carubbi Neto <rcarubbi@gmail.com>
2023-04-08 01:41:12 +0100

    add lesson 23

---
+++ 23-RayCaster/src/script.js
@@ -0,0 +1,232 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import gsap from 'gsap';
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+/**
+ * Load Models
+ */
+let duckModel = null;
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load(
+	'/models/Duck/glTF-Binary/Duck.glb',
+	(gltf) => {
+		duckModel = gltf.scene;
+		duckModel.position.y = -1.2;
+		scene.add(duckModel);
+	},
+	(event) => {},
+	(err) => {}
+);
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+	new THREE.SphereGeometry(0.5, 16, 16),
+	new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+object1.position.x = -2;
+
+const object2 = new THREE.Mesh(
+	new THREE.SphereGeometry(0.5, 16, 16),
+	new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+const object3 = new THREE.Mesh(
+	new THREE.SphereGeometry(0.5, 16, 16),
+	new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+object3.position.x = 2;
+
+scene.add(object1, object2, object3);
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster();
+
+// const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// const rayDirection = new THREE.Vector3(10, 0, 0);
+// rayDirection.normalize();
+
+// raycaster.set(rayOrigin, rayDirection);
+// const intersect = raycaster.intersectObject(object2);
+// console.log(intersect);
+
+// const intersects = raycaster.intersectObjects([object1, object2, object3]);
+// console.log(intersects);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
+directionalLight.position.set(1, 2, 3);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+	// Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
+	0.1,
+	100
+);
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2();
+window.addEventListener('mousemove', (event) => {
+	mouse.x = (event.clientX / sizes.width) * 2 - 1;
+	mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+});
+
+window.addEventListener('click', () => {
+	if (currentIntersect) {
+		switch (currentIntersect.object) {
+			case object1:
+				console.log('click on sphere 1');
+				break;
+			case object2:
+				console.log('click on sphere 2');
+
+				break;
+			case object3:
+				console.log('click on sphere 3');
+
+				break;
+		}
+	}
+});
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+const origin = new THREE.Vector3(-3, 0, 0);
+const direction = new THREE.Vector3(10, 0, 0);
+
+let currentIntersect = null;
+
+const tick = () => {
+	const elapsedTime = clock.getElapsedTime();
+
+	object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+	object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+	object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+	raycaster.setFromCamera(mouse, camera);
+	object1.material.color.set(0xff0000);
+	object2.material.color.set(0xff0000);
+	object3.material.color.set(0xff0000);
+
+	// direction.normalize();
+
+	// raycaster.set(origin, direction);
+
+	const intersects = raycaster.intersectObjects([object1, object2, object3]);
+	for (const intersect of intersects) {
+		intersect.object.material.color.set(0x0000ff);
+	}
+
+	if (intersects.length) {
+		if (currentIntersect === null) {
+			console.log('mouse enter');
+		}
+		currentIntersect = intersects[0];
+	} else {
+		if (currentIntersect) {
+			console.log('mouse leave');
+		}
+		currentIntersect = null;
+	}
+
+	if (duckModel) {
+		const duckIntersect = raycaster.intersectObject(duckModel);
+		if (duckIntersect.length) {
+			// duckModel.scale.set(1.2, 1.2, 1.2);
+			console.log(duckModel);
+			gsap.to(duckModel.scale, {
+				duration: 1.5,
+				x: 1.4,
+				y: 1.4,
+				z: 1.4,
+			});
+		} else {
+			gsap.to(duckModel.scale, {
+				duration: 1.5,
+				x: 1,
+				y: 1,
+				z: 1,
+			});
+
+			// duckModel.scale.set(1, 1, 1);
+		}
+	}
+	// Update controls
+	controls.update();
+
+	// Render
+	renderer.render(scene, camera);
+
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick);
+};
+
+tick();
